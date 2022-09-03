9af85c5da05451393b7f85172d9512bf1d0742c7
stb gz <eginez@gmail.com>
2022-09-03 06:30:09 +0000

    ki

--- .space.kts
+++ .space.kts
@@ -1,20 +1,10 @@
-job("Example") {
-    // check-out repo-2 to /mnt/space/work/repo-2
-    git("repo-2")
-    // check-out repo-3 to /mnt/space/work/thirdRepo
-    git("repo-3") {
-        cloneDir = "thirdRepo"
-    }
-    container(displayName = "Show dir", image = "openjdk:latest") {
+job("Example shell script") {
+    container(displayName = "Say Hello", image = "ubuntu") {
         shellScript {
             content = """
-                echo Directory structure
-                ls -R /mnt
-                echo Working dir is
-                pwd
+               ls -larth
+                echo World!
             """
         }
     }
-    // If the main repository is 'main-repo', then
-    // working dir is /mnt/space/work/main-repo
 }
